2fda67c994c3033cc79d9e47b789b53e98ea4ae8
Wade-Coplen <wadecoplen@gmail.com>
2020-04-18 10:44:13 -0700

    3- added migrations up/downs

---
+++ migrations/20200418102935_entities_db.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('project', tbl => {
+      tbl.increments();
+      tbl.text('name', 128).notNullable().unique();
+      tbl.text('description', 128);
+      tbl.boolean('project_completed', false).notNullable;
+  })
+  .createTable('resource', tbl => {
+      tbl.increments();
+      tbl.text('name', 128).notNullable().unique();
+      tbl.text('description', 128);
+  })
+  .createTable('task', tbl => {
+      tbl.increments();
+      tbl.text('description', 128).notNullable();
+      tbl.text('notes', 128);
+      tbl.boolean('task_complete', false);
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('project')
+  .dropTableIfExists('resource')
+  .dropTableIfExists('task')
+};
